860c6135228707915c2bf7c52fad76e4ffe91353
maiabrr <maiabelenr@gmail.com>
2024-05-22 17:45:17 -0400

    react blocks

---
+++ src/Components/Advertisement.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Advertisement.css';
+
+const Advertisement = () => {
+  return <div className="advertisement"></div>;
+}
+
+export default Advertisement;
---
+++ src/Components/SubContent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SubContent.css';
+
+const SubContents = () => {
+  return <div className="subcontent"></div>;
+}
+
+export default SubContents;
